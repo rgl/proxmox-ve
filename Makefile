38700a108c3d33a39e91689616cc8b3e050e9cf9
Rui Lopes <rgl@ruilopes.com>
2017-05-26 07:43:49 +0100

    fix the installation of the VirtualBox 5.1.20 Guest Additions

--- Makefile
+++ Makefile
@@ -7,14 +7,14 @@ build-virtualbox: proxmox-ve-amd64-virtualbox.box
 
 proxmox-ve-amd64-libvirt.box: *.sh proxmox-ve.json Vagrantfile.template
 	rm -f proxmox-ve-amd64-libvirt.box
-	PACKER_KEY_INTERVAL=10ms packer build -only=proxmox-ve-amd64-libvirt proxmox-ve.json
+	PACKER_KEY_INTERVAL=10ms packer build -only=proxmox-ve-amd64-libvirt -on-error=abort proxmox-ve.json
 	@echo Box successfully built!
 	@echo to add it to vagrant run:
 	@echo vagrant box add -f proxmox-ve-amd64 proxmox-ve-amd64-libvirt.box
 
 proxmox-ve-amd64-virtualbox.box: *.sh proxmox-ve.json Vagrantfile.template
 	rm -f proxmox-ve-amd64-virtualbox.box
-	packer build -only=proxmox-ve-amd64-virtualbox proxmox-ve.json
+	packer build -only=proxmox-ve-amd64-virtualbox -on-error=abort proxmox-ve.json
 	@echo Box successfully built!
 	@echo to add it to vagrant run:
 	@echo vagrant box add -f proxmox-ve-amd64 proxmox-ve-amd64-virtualbox.box
--- provision.sh
+++ provision.sh
@@ -37,13 +37,18 @@ popd
 if [ -n "$(lspci | grep VirtualBox)" ]; then
 # install the VirtualBox Guest Additions.
 # this will be installed at /opt/VBoxGuestAdditions-VERSION.
-# REMOVE_INSTALLATION_DIR=0 is to fix a bug in VBoxLinuxAdditions.run.
-# See http://stackoverflow.com/a/25943638.
+# NB You can unpack the VBoxLinuxAdditions.run file contents with:
+#       VBoxLinuxAdditions.run --target /tmp/VBoxLinuxAdditions.run.contents --noexec
+# NB REMOVE_INSTALLATION_DIR=0 is to fix a bug in VBoxLinuxAdditions.run.
+#    See http://stackoverflow.com/a/25943638.
 apt-get -y -q install gcc dkms pve-headers
 mkdir -p /mnt
 mount /dev/sr1 /mnt
 while [ ! -f /mnt/VBoxLinuxAdditions.run ]; do sleep 1; done
-REMOVE_INSTALLATION_DIR=0 /mnt/VBoxLinuxAdditions.run --target /tmp/VBoxGuestAdditions
+# NB we ignore exit code 2 (cannot find vboxguest module) because of what
+#    seems to be a bug in VirtualBox 5.1.20. there isn't actually a problem
+#    loading the module.
+REMOVE_INSTALLATION_DIR=0 /mnt/VBoxLinuxAdditions.run --target /tmp/VBoxGuestAdditions || [ $? -eq 2 ]
 rm -rf /tmp/VBoxGuestAdditions
 umount /mnt
 eject /dev/sr1
