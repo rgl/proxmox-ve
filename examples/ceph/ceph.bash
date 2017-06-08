32356a13e602b73dbeb9e4b33314df2b97c80374
pixel fairy <pixelfairy@gmail.com>
2017-06-08 01:40:52 -0700

    Made examples a folder
    examples is now a folder with ceph and the original lxc example.

---
+++ examples/ceph/ceph.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -exu
+
+net=$1
+ip=$2
+
+echo y | pveceph install
+pveceph init --network ${net}0/24
+
+# allow ceph to run on a single node
+cp /etc/ceph/ceph.conf /etc/ceph/ceph.conf.bak
+awk 'NR==9{print "         osd crush chooseleaf type = 0"}1' \
+  /etc/ceph/ceph.conf.bak > /etc/ceph/ceph.conf
+
+pveceph createmon
+for i in b c d;do
+  pveceph createosd /dev/vd$i
+done
+
+mkdir /etc/pve/priv/ceph
+cp /etc/ceph/ceph.client.admin.keyring /etc/pve/priv/ceph/rbd.keyring
+cat <<EOF >>/etc/pve/storage.cfg
+
+rbd: rbd
+        monhost ${ip}
+        content images
+        krbd 0
+        pool rbd
+        username admin
+EOF
