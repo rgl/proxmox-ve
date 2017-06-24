c25fd3bdd36a25a5be7c202058af504993bffece
Rui Lopes <rgl@ruilopes.com>
2017-06-24 11:40:34 +0100

    add support for the pt_PT locale

--- upgrade.sh
+++ upgrade.sh
@@ -36,6 +36,11 @@ auto vmbr0
 iface vmbr0 inet manual
 EOF
 
+# add support for the pt_PT locale.
+sed -i -E 's,.+(pt_PT.UTF-8 .+),\1,' /etc/locale.gen
+locale-gen
+locale -a
+
 # set the keyboard layout.
 apt-get install -y console-data
 cat >/etc/default/keyboard <<'EOF'
