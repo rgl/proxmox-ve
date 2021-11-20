34c4801337a9277764112dce50e9a42e4bc4f41f
Rui Lopes <rgl@ruilopes.com>
2021-11-20 08:00:08 +0000

    use the correct os version codename in pve.list

--- upgrade.sh
+++ upgrade.sh
@@ -7,7 +7,7 @@ export DEBIAN_FRONTEND=noninteractive
 # switch to the non-enterprise repository.
 # see https://pve.proxmox.com/wiki/Package_Repositories
 rm -f /etc/apt/sources.list.d/pve-enterprise.list
-echo 'deb http://download.proxmox.com/debian/pve buster pve-no-subscription' >/etc/apt/sources.list.d/pve.list
+echo "deb http://download.proxmox.com/debian/pve $(. /etc/os-release && echo "$VERSION_CODENAME") pve-no-subscription" >/etc/apt/sources.list.d/pve.list
 
 # switch the apt mirror from us to nl.
 sed -i -E 's,ftp\.us\.debian,ftp.nl.debian,' /etc/apt/sources.list
