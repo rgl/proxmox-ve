2ba05f1be1148a4d323e383c042ecb96039c0b71
Rui Lopes <rgl@ruilopes.com>
2023-06-23 08:17:15 +0100

    upgrade to proxmox-ve 8.0-1

--- provisioners/upgrade.sh
+++ provisioners/upgrade.sh
@@ -7,7 +7,9 @@ export DEBIAN_FRONTEND=noninteractive
 # switch to the non-enterprise repository.
 # see https://pve.proxmox.com/wiki/Package_Repositories
 rm -f /etc/apt/sources.list.d/pve-enterprise.list
+rm -f /etc/apt/sources.list.d/ceph.list
 echo "deb http://download.proxmox.com/debian/pve $(. /etc/os-release && echo "$VERSION_CODENAME") pve-no-subscription" >/etc/apt/sources.list.d/pve.list
+echo "deb http://download.proxmox.com/debian/ceph-quincy $(. /etc/os-release && echo "$VERSION_CODENAME") no-subscription" >/etc/apt/sources.list.d/ceph.list
 
 # switch the apt mirror from us to nl.
 sed -i -E 's,ftp\.us\.debian,ftp.nl.debian,' /etc/apt/sources.list
